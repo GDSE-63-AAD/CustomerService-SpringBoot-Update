baa34f19c5b03a082502e122460a3222237706da
ThanuraSilva <thanurasilva1989@gmail.com>
2023-09-16 21:07:13 +0530

    Property file and main-class modified

--- src/main/java/lk/ijse/gdse/aad/CustomerService/CustomerServiceApplication.java
+++ src/main/java/lk/ijse/gdse/aad/CustomerService/CustomerServiceApplication.java
@@ -1,10 +1,16 @@
 package lk.ijse.gdse.aad.CustomerService;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
