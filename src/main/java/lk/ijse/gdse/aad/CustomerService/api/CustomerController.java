3ebf9d779a299c101277f00c1b66ed899ea255f5
ThanuraSilva <thanurasilva1989@gmail.com>
2023-09-24 23:36:29 +0530

    Include validations

--- src/main/java/lk/ijse/gdse/aad/CustomerService/api/CustomerController.java
+++ src/main/java/lk/ijse/gdse/aad/CustomerService/api/CustomerController.java
@@ -1,41 +1,58 @@
 package lk.ijse.gdse.aad.CustomerService.api;
 
+import jakarta.validation.Valid;
 import lk.ijse.gdse.aad.CustomerService.dto.CustomerDTO;
 import lk.ijse.gdse.aad.CustomerService.service.CustomerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/v1/customer")
 @CrossOrigin("*")
 public class CustomerController {
     private final CustomerService customerService;
-    public CustomerController(CustomerService customerService){
+
+    public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping(consumes = "application/json",produces = "application/json")
-    CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDto){
-        //ToDo: Error handling
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    CustomerDTO saveCustomer(@Valid @RequestBody CustomerDTO customerDto, Errors errors) {
+        //Validation
+        if(errors.hasFieldErrors()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,errors
+                    .getFieldErrors().get(0).getDefaultMessage());
+        }
         return customerService.saveCustomer(customerDto);
     }
 
-    @GetMapping(value = "{code}",produces = MediaType.APPLICATION_JSON_VALUE)
-    CustomerDTO getSelectedCustomer( @PathVariable String code){
-        return customerService.getSelectedCustomer(code);
+    @GetMapping(value = "{code:[A-Fa-f0-9\\-]{36}}", produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<CustomerDTO> getSelectedCustomer(@Valid  @PathVariable String code) {
+        CustomerDTO selectedCustomer = customerService.getSelectedCustomer(code);
+        return selectedCustomer != null ? ResponseEntity.ok(selectedCustomer) : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
-    @DeleteMapping(value =  "{code}")
-    void deleteCustomer ( @PathVariable String code){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "{code:[A-Fa-f0-9\\-]{36}}")
+    void deleteCustomer(@Valid @PathVariable String code) {
         customerService.deleteCustomer(code);
     }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping(value = "{code:[A-Fa-f0-9\\-]{36}}" )
+    void updateCustomer(@Valid @PathVariable String code, @RequestBody CustomerDTO customer,Errors errors) {
+        //Validation
+        if(errors.hasFieldErrors()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    errors.getFieldErrors().get(0).getDefaultMessage());
+        }
 
-    @PatchMapping(value = "{code}")
-    void updateCustomer(@PathVariable String code, @RequestBody CustomerDTO customer){
-        //ToDo: Error handling
         customer.setCustomerCode(code);
         customerService.updateCustomer(customer);
     }
+
 }
--- src/main/java/lk/ijse/gdse/aad/CustomerService/dto/CustomerDTO.java
+++ src/main/java/lk/ijse/gdse/aad/CustomerService/dto/CustomerDTO.java
@@ -1,4 +1,8 @@
 package lk.ijse.gdse.aad.CustomerService.dto;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +13,15 @@ import java.io.Serializable;
 @NoArgsConstructor
 @Data
 public class CustomerDTO implements Serializable {
+    @Null(message = "Customer Code will auto generate")
     private String customerCode;
+    @NotNull(message = "Customer name cannot be empty")
+    @Pattern(regexp = "[A-Za-z ]+")
     private String customerName;
+    @NotNull(message = "Customer address cannot be empty")
     private String customerAddress;
+    @NotNull(message = "Customer email cannot be empty")
+    @Email
     private String customerEmail;
 
     public CustomerDTO(String customerName, String customerAddress, String customerEmail) {
